cacf527d16a12edd46f8fab1542ac7fadf645ac9
SGMohan <iamsgmohan@gmail.com>
2025-11-28 00:34:15 +0530

    Updated

--- Server/config/nodemailer.js
+++ Server/config/nodemailer.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
   port: 465,
   secure: true,
   auth: {
