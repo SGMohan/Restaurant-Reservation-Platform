a7ee0580ec08041c8c7fcf02147d13e4867ac199
SGMohan <iamsgmohan@gmail.com>
2025-11-28 00:29:24 +0530

    Updated

--- Server/config/nodemailer.js
+++ Server/config/nodemailer.js
@@ -3,6 +3,8 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER, // your Gmail address
     pass: process.env.EMAIL_PASS, // your App Password (not direct Gmail password)
