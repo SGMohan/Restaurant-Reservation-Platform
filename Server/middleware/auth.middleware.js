6cc4b7f206d78d83999efef8cd8533aa992a179d
SGMohan <iamsgmohan@gmail.com>
2025-11-27 21:58:54 +0530

    updated

--- Server/middleware/auth.middleware.js
+++ Server/middleware/auth.middleware.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
     return res.status(401).json({ message: "No token provided" });
   }
   const token = authHeader.split(" ")[1]; // get token part
